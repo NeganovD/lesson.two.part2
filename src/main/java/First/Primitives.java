ef44de8494dba16a92e8ba17e31a39c4388aa1df
Dmitriy <Dmitriy.Neganov@vsegda-da.com>
2023-10-31 21:46:35 +0400

    push lesson.two.part2

--- src/main/java/First/Primitives.java
+++ src/main/java/First/Primitives.java
@@ -1,14 +1,12 @@
 package First;
 
 public class Primitives {
-    public static void main(String[] args) {
-        int intPrimitives = 2147483647; //maxValue
-        double doublePrimitives = 3.1;
-        char charPrimitives = 'A';
-        boolean booleanPrimitives = true;
-        byte bytePrimitives = 127; //maxValue
-        short shortPrimitives = 32767; //maxValue
-        long longPrimitives = 9223372036854775807L; //maxValue
-        float floatPrimitives = 3.4e+38f; //maxValue
-    }
+    final int intPrimitives = 2147483647; //maxValue
+    final double doublePrimitives = 3.1;
+    final char charPrimitives = 'A';
+    final boolean booleanPrimitives = true;
+    final byte bytePrimitives = 127; //maxValue
+    final short shortPrimitives = 32767; //maxValue
+    final long longPrimitives = 9223372036854775807L; //maxValue
+    final float floatPrimitives = 3.4e+38f; //maxValue
 }
--- src/main/java/First/Wrappers.java
+++ src/main/java/First/Wrappers.java
@@ -1,14 +1,13 @@
 package First;
 
 public class Wrappers {
-    public static void main(String[] args) {
-        Integer integerWrapper = -2147483648; //minValue
-        Double doubleWrapper = 3.54;
-        Character charWrapper = 'a';
-        Boolean booleanWrapper = false;
-        Byte byteWrapper = -128; //minValue
-        Short shortWrapper = -32768; //minValue
-        Long longWrapper = -9223372036854775808L; //minValue
-        Float floatWrapper = 1.4e-45f; //minValue
-    }
+
+    final Integer integerWrapper = -2147483648; //minValue
+    final Double doubleWrapper = 3.54;
+    final Character charWrapper = 'a';
+    final Boolean booleanWrapper = false;
+    final Byte byteWrapper = -128; //minValue
+    final Short shortWrapper = -32768; //minValue
+    final Long longWrapper = -9223372036854775808L; //minValue
+    final Float floatWrapper = 1.4e-45f; //minValue
 }
